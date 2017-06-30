8619c3d794ee5213299f92487f665f131f49a5d3
FlightControl <sven.van.de.velde@telenet.be>
2017-06-30 10:38:51 +0200

    New versions

--- ACL - Airbase Cleaner/ACL-001 - Airbase CleanUp/ACL-001 - Airbase CleanUp.lua
+++ ACL - Airbase Cleaner/ACL-001 - Airbase CleanUp/ACL-001 - Airbase CleanUp.lua
@@ -2,9 +2,11 @@
 
 
 
-Clean = CLEANUP:New( 'CLEAN_BATUMI', 180 )
+Clean = CLEANUP:New( AIRBASE.Caucasus.Batumi )
 
-SpawnRU = SPAWN:New( 'RU Attack Heli Batumi'):InitLimit( 2, 20 ):SpawnScheduled( 2, 0.2 )
+SpawnRU = SPAWN:New( 'RU Attack Heli Batumi'):InitLimit( 4, 120 ):SpawnScheduled( 10, 0.2 )
 
-SpawnUS = SPAWN:New( 'US Attack Heli Batumi'):InitLimit( 2, 20 ):SpawnScheduled( 2, 0.2 )
+SpawnUS = SPAWN:New( 'US Attack Heli Batumi'):InitLimit( 4, 120 ):SpawnScheduled( 10, 0.2 )
+
+SpawnTransport = SPAWN:New( "US Transport"):SpawnScheduled( 60, 0 )
 
